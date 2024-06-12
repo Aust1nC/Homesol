4ba9cb359f93450b4826053227d13b702eb9cfd5
Austin Chen <austinchen_zy@hotmail.com>
2024-06-12 22:52:10 +0100

    Finished slider

--- frontend/src/app/components/stepper/stepper.component.spec.ts
+++ frontend/src/app/components/stepper/stepper.component.spec.ts
@@ -1,18 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { stepperComponent } from './stepper.component';
 
-import { StepsComponent } from './stepper.component';
-
-describe('StepsComponent', () => {
-  let component: StepsComponent;
-  let fixture: ComponentFixture<StepsComponent>;
+describe('stepperComponent', () => {
+  let component: stepperComponent;
+  let fixture: ComponentFixture<stepperComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [StepsComponent]
-    })
-    .compileComponents();
+      declarations: [stepperComponent],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(StepsComponent);
+    fixture = TestBed.createComponent(stepperComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
