75456dde903be6e0e99a98fb7989ae3fd2521618
Austin Chen <austinchen_zy@hotmail.com>
2024-06-14 00:47:03 +0100

    Added footer and adjust page bottom padding

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
   template: `
     <app-header></app-header>
     <router-outlet></router-outlet>
+    <app-footer> </app-footer>
   `,
   styles: [],
 })
