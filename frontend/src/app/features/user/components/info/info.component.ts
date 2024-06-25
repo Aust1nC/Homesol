c6f42bfcb96423064857979a50575dc852772921
Austin Chen <austinchen_zy@hotmail.com>
2024-06-26 00:36:35 +0100

    Profile Page form group setup

--- frontend/src/app/features/user/components/info/info.component.ts
+++ frontend/src/app/features/user/components/info/info.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrl: './info.component.css'
+  styleUrl: './info.component.css',
 })
 export class InfoComponent {
+  constructor() {}
 
+  bioSection = new FormGroup({
+    username: new FormControl<string>('', [
+      Validators.minLength(3),
+      Validators.required,
+    ]),
+    firstName: new FormControl<string>('', [Validators.required]),
+    lastName: new FormControl<string>('', [Validators.required]),
+    createdAt: new FormControl<string>(''),
+  });
+
+  subscriptionSection = new FormGroup({
+    subscription: new FormControl<string>(''),
+    startDate: new FormControl<string>(''),
+    endDate: new FormControl<string>(''),
+  });
 }
--- frontend/src/app/features/user/user.module.ts
+++ frontend/src/app/features/user/user.module.ts
@@ -5,9 +5,10 @@ import { UserRoutingModule } from './user-routing.module';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { DrawerComponent } from './components/drawer/drawer.component';
 import { InfoComponent } from './components/info/info.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProfileComponent, DrawerComponent, InfoComponent],
-  imports: [CommonModule, UserRoutingModule],
+  imports: [CommonModule, UserRoutingModule, ReactiveFormsModule],
 })
 export class UserModule {}
